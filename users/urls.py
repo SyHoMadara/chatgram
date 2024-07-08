e3ed61e05d8d0de2d0dbfd0f73d86af6075da8f9
S.Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2024-07-08 04:55:08 +0330

    setting up urls for users and token with swagger api documentation

---
+++ users/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+
+urlpatterns = [
+    # path("register/", RegisterUserView.as_view(), name="register"),
+    # path("login/", LoginUserView.as_view(), name="login"),
+    # path("logout/", LogoutUserView.as_view(), name="logout"),
+    # path("profile/", ProfileUserView.as_view(), name="profile"),
+    # path("users/<int:pk>/", UserDetailView.as_view(), name="user-detail"),
+
+]
