b9b83cc49ea65911d3fe690afc9fb47a86f49dfa
S.Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2024-07-08 04:00:04 +0330

    fix user models.py

--- users/models.py
+++ users/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.hashers import make_password
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -31,6 +32,8 @@ class CustomUserManager(BaseUserManager):
 class User(AbstractBaseUser, PermissionsMixin):
     username = None
     email = models.EmailField(_("email address"), unique=True)
+    first_name = models.CharField(_("first name"), max_length=30, blank=True)
+    last_name = models.CharField(_("last name"), max_length=30, blank=True)
     is_staff = models.BooleanField(_("staff status"), default=False)
     is_active = models.BooleanField(_("active"), default=True)
     is_verified = models.BooleanField(_("verified"), default=False)
@@ -47,6 +50,15 @@ class User(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _("user")
         verbose_name_plural = _("users")
+
+    def delete(self, using=None, keep_parents=False):
+        self.is_deleted = True
+        self.save()
+
+    def set_password(self, raw_password):
+        validate_password(raw_password, self)
+        self.password = make_password(raw_password)
+        self.save()
 
     def __str__(self):
         return self.email
