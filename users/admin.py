8f3dd50a8404432f2a0092c8cd741ede0ee0a58e
S.Hosein Sarafrazi <sh.sarafrazi80@gmail.com>
2024-07-08 06:25:30 +0330

    admin profile

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name')}),
+        ('Permissions', {'fields': ('is_staff',)}),
+        # ('Dates', {'fields': ('last_login', 'date_joined')}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("email",),
+            },
+        ),
+    )
+    # readonly_fields = ['date_joined', 'last_login']
+    list_display = ('email', 'first_name', 'last_name')
+    search_fields = ('email', 'first_name', 'last_name')
+    ordering = ('email',)
+    filter_horizontal = ()
+
+
+admin.site.register(User, CustomUserAdmin)
